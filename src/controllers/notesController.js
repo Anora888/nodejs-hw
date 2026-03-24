d72fe93322dab95599d61b654f02f4e520b84212
Anora888 <anoramaomao@gmail.com>
2026-03-24 18:40:11 +0200

    added fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -24,9 +24,7 @@ export const createNote = async (req, res) => {
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-  });
+  const note = await Note.findOneAndDelete({ _id: noteId });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -38,9 +36,11 @@ export const deleteNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    new: true,
-  });
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId },
+    req.body,
+    { returnDocument: 'after' } 
+  );
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
